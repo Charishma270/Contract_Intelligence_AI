30d686d1dd3d690cbb0fdc756533ee560f37f968
tisha1406 <tis007.ts@gmail.com>
2026-05-11 15:57:44 +0530

    week1-mon: implemented semantic retrieval pipeline with FAISS

---
+++ rag/retrieval/embedder.py
@@ -0,0 +1,8 @@
+from sentence_transformers import SentenceTransformer
+
+# Load embedding model
+model = SentenceTransformer("all-MiniLM-L6-v2")
+
+def generate_embedding(text):
+    embedding = model.encode(text, normalize_embeddings=True)
+    return embedding
