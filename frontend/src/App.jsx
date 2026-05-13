5e8da9550d6fc048d7df2447f4ddc8f793d5800c
Mukt Patel <your-github-muktpatel60@gmail.com>
2026-05-13 08:07:52 +0530

    week1-wed: upload page UI created

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Dashboard from "./pages/Dashboard";
+import Upload from "./pages/Upload"; 
 
 function App() {
   return (
@@ -7,6 +8,10 @@ function App() {
       <Routes>
         <Route path="/" element={<Dashboard />} />
         <Route path="/dashboard" element={<Dashboard />} />
+        
+        {}
+        <Route path="/upload" element={<Upload />} />
+        
       </Routes>
     </BrowserRouter>
   );
